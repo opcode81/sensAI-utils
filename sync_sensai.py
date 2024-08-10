145c47382248a98bb48f20265f430d43e4f050de
Dominik Jain <dominik.jain@outlook.com>
2024-08-10 14:24:13 +0200

    Handle sensai.__init__ when synchronising

--- sync_sensai.py
+++ sync_sensai.py
@@ -6,7 +6,8 @@ def sync():
     SRC_DIRECTORY = os.path.join("..", "sensAI", "src", "sensai")
     LIB_DIRECTORY = os.path.join("src", "sensai")
 
-    shutil.move(os.path.join(LIB_DIRECTORY, "util", "__init__.py"), "__init__.py.bak")
+    shutil.move(os.path.join(LIB_DIRECTORY, "util", "__init__.py"), "__init__.py.util.bak")
+    shutil.move(os.path.join(LIB_DIRECTORY, "__init__.py"), "__init__.py.sensai.bak")
 
     if os.path.exists(LIB_DIRECTORY):
         shutil.rmtree(LIB_DIRECTORY)
@@ -14,7 +15,8 @@ def sync():
     shutil.copytree(os.path.join(SRC_DIRECTORY, "util"), os.path.join(LIB_DIRECTORY, "util"))
     os.unlink(os.path.join(LIB_DIRECTORY, "util", "__init__.py"))
 
-    shutil.move("__init__.py.bak", os.path.join(LIB_DIRECTORY, "util", "__init__.py"))
+    shutil.move("__init__.py.util.bak", os.path.join(LIB_DIRECTORY, "util", "__init__.py"))
+    shutil.move("__init__.py.sensai.bak", os.path.join(LIB_DIRECTORY, "__init__.py"))
 
 
 if __name__ == "__main__":
